55c638e885fe4b2d1eb0b4aef25bb641f3af0ac2
Monikaa2611 <149382361+Monikaa2611@users.noreply.github.com>
2023-11-06 14:02:29 +0100

    chore: add remove user function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dane import users_list
 
 def add_user_to(users_list:list) -> None:
-    # 3 razy shift enter
+
     """
     add object to list
     :param users_list: list - user list
@@ -11,13 +11,37 @@ def add_user_to(users_list:list) -> None:
     posts = input('dodaj liczbę postów')
     users_list.append({'name': name, 'posts': posts})
 
-add_user_to(users_list)
-add_user_to(users_list)
-add_user_to(users_list)
+def remove_user_from(users_list: list) -> None:
+    """
+    remove object from list
+    :param users_list: list -> user list
+    :return: None
+    """
+    tmp_list = []
+    name = input('podaj imię użytkownika do usunięcia: ')
+    for user in users_list:
+        if user["name"]== name:
+            tmp_list.append(user)
+    print('Znaleziono użytkowników:')
+    print('0: Usuń wszytskich znalezionych użytkowników')
+    for numerek, user_to_be_remowed in enumerate(tmp_list):
+        print(f'{numerek+1}. {user_to_be_remowed}')
+    numer = int(input(f'Wybierz numer użytkownika do usunięcia:'))
+    if numer == 0:
+        for user in users_list:
+            if user['name'] == name:
+                users_list.remove(user)
+    else:
+        users_list.remove(tmp_list[numer-1])
+#     print(numer)
+#     print(tmp_list[(numer-1)])
+#     users_list.remove(tmp_list[numer-1])
+#
+remove_user_from(users_list)
 
+# print(users_list)
 for user in users_list:
     print(f'Twój znajomy {user["name"]} dodał {user["posts"]}')
-
 
 
 

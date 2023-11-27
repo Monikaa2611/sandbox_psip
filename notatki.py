43d750d04157564b0cc58e8e8c83e4f3344834cd
Monikaa2611 <149382361+Monikaa2611@users.noreply.github.com>
2023-11-27 13:14:58 +0100

    chore: get_coordinates_from_wiki_page

--- notatki.py
+++ notatki.py
@@ -1,21 +1,21 @@
-from dane import users_list
+from bs4 import BeautifulSoup
+import requests
+
+nazwy_miejscowosci = ['Kraków','Mińsk Mazowiecki', 'Szczecin']
+def get_coordinates_of(city:str)->list[float,float]:
+
+    adres_URL = f'https://pl.wikipedia.org/wiki/{city}'
+    response = requests.get(url=adres_URL)
+    response_html = BeautifulSoup(response.text, 'html.parser')
 
 
 
+    response_html_latitude = response_html.select('.latitude')[1].text
+    response_html_latitude = float(response_html_latitude.replace(',','.'))
+    response_html_longitude = response_html.select('.longitude')[1].text
+    response_html_longitude = float(response_html_longitude.replace(',','.'))
 
+    return [response_html_latitude, response_html_longitude]
 
-def update_users(users_list: list[dict,dict]) -> None:
-    nick_of_users = input('podaj nick uzytkownika do modyfikacji')
-    print(nick_of_users)
-    for users in users_list:
-        if users['nick'] == nick_of_users:
-           print('Znaleziono !!')
-           users['name'] = input('podaj nowe imie: ')
-           users['nick'] = input('podaj nowa ksywe: ')
-           users['posts'] = int(input('podaj liczbe postow: '))
-
-update_users(users_list)
-for users in users_list:
-    print(users)
-
-
+for item in nazwy_miejscowosci:
+    print(get_coordinates_of(item))
